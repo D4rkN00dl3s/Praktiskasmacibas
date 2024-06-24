4255bcd3fbb9907116f1f18d429bf7b1e431028b
D4rkN00dl3s <martin.osit.2057@gmail.com>
2024-06-24 16:49:53 +0300

    Major Update
    Added showcase pictures for Java and Python and design changes, adaptiveness to screen resolution

--- 2majaslapa/showcase.js
+++ 2majaslapa/showcase.js
@@ -1,26 +1,31 @@
-function revealElement(elementId) {
-    var element = document.getElementById(elementId);
-    if (element) {
-        element.style.display = 'inline';
-    } else {
-        console.error('Element with id ' + elementId + ' not found.');
-    }
-}
-
-function hideElement(elementId) {
-    var element = document.getElementById(elementId);
-    if (element) {
-        element.style.display = 'none';
-    } else {
-        console.error('Element with id ' + elementId + ' not found.');
-    }
-}
-
+// Efficient event listener for showing/hiding header
 window.addEventListener('scroll', function() {
-    var element = document.getElementById('header');
-    if (window.scrollY >= 208) {
-      element.style.display = 'inline';
+    var header = document.getElementById('header');
+    var triggerHeight = 208; // Adjust as needed
+
+    if (window.scrollY >= triggerHeight) {
+      header.style.display = 'block';
     } else {
-      element.style.display = 'none';
+      header.style.display = 'none';
     }
   });
+
+  // Function to reveal element by ID
+  function revealElement(elementId) {
+    var element = document.getElementById(elementId);
+    if (element) {
+      element.style.display = 'block'; // or 'inline' as per your requirement
+    } else {
+      console.error('Element with id ' + elementId + ' not found.');
+    }
+  }
+
+  // Function to hide element by ID
+  function hideElement(elementId) {
+    var element = document.getElementById(elementId);
+    if (element) {
+      element.style.display = 'none';
+    } else {
+      console.error('Element with id ' + elementId + ' not found.');
+    }
+  }
